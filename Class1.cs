84815be491a03f41a9b91cdb3ddf8c5e9f9df1c3
gmarinou84-sys <g.marinou84@gmail.com>
2026-07-12 16:26:31 +0300

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,42 +1,13 @@
 using System;
-using System.Drawing;
-using GooseShared;
+using GooseModdingAPI;
+using SamEngine;
 
 namespace TurboDrifterGoose
 {
     public class ModMain : IMod
     {
-        private Vector2 driftVelocity = new Vector2(0, 0);
-        private Random rand = new Random();
-
-        public void Init(GooseAPI api)
+        public void Init()
         {
-            api.OnUpdate += ApplyDriftPhysics;
-        }
-
-        private void ApplyDriftPhysics(GooseAPI api)
-        {
-            if (api.currentTask == GooseAPI.Task.Walking || api.currentTask == GooseAPI.Task.Charging)
-            {
-                api.currentSpeed = 350; 
-
-                driftVelocity.X = (driftVelocity.X * 0.92f) + (api.targetDirection.X * 0.08f);
-                driftVelocity.Y = (driftVelocity.Y * 0.92f) + (api.targetDirection.Y * 0.08f);
-
-                api.position.X += driftVelocity.X * api.currentSpeed * 0.016f;
-                api.position.Y += driftVelocity.Y * api.currentSpeed * 0.016f;
-
-                if (rand.Next(0, 100) < 25) 
-                {
-                    Color neonColor = Color.FromArgb(rand.Next(0, 256), rand.Next(100, 256), rand.Next(200, 256));
-                    api.SpawnParticle(api.position, 1.5f, neonColor);
-                }
-            }
-            else
-            {
-                driftVelocity.X = 0;
-                driftVelocity.Y = 0;
-            }
         }
     }
 }
